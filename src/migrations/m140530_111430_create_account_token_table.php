eaa24261c860097ec581b5b960c44c40a3a4c9a7
Christoffer Niska <christoffer.niska@nordsoftware.com>
2014-06-04 13:02:17 +0300

    Change datetime fields to timestamps and add missing field to the account_token table

--- src/migrations/m140530_111430_create_account_token_table.php
+++ src/migrations/m140530_111430_create_account_token_table.php
@@ -11,7 +11,8 @@ class m140530_111430_create_account_token_table extends CDbMigration
                 'accountId' => 'int NOT NULL',
                 'type' => 'string NOT NULL',
                 'token' => 'string NOT NULL',
-                'expiresAt' => 'datetime NOT NULL',
+                'createdAt' => 'timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP',
+                'expiresAt' => 'timestamp NOT NULL',
                 'status' => "integer NOT NULL DEFAULT '0'",
                 'UNIQUE KEY accountId_token (accountId, token)',
             )
